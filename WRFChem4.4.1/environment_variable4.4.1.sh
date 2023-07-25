d26728ea3f35121e954d20c8fc1e9a6a64b42045
Romel Príncipe <57165643+RomelP@users.noreply.github.com>
2023-07-25 16:44:32 -0500

    Update environment_variable4.4.1.sh
    Reparo de falla1

--- WRFChem4.4.1/environment_variable4.4.1.sh
+++ WRFChem4.4.1/environment_variable4.4.1.sh
@@ -4,44 +4,47 @@
 #________________________________________________
 #para abrir el archivo a editar:
 
-#sudo vim ~/.bashrc
+#sudo vim ~/.bashrc # for all users
+# vim ~/.profile # for user alone
 #________________________________________________
 #Incluir al final las siguientes lineas:
-
-(...)
-
+#________________________________________________
 # Compilers
 export DIR=/home/Build_WRF/LIBRARIES
 export CC=gcc
 export CXX=g++
 export FC=gfortran
+#________________________
 # hdf5
 export PATH=$DIR/hdf5/bin:$PATH
 export LD_LIBRARY_PATH=$DIR/hdf5/lib:$LD_LIBRARY_PATH
+#________________________
 # NETCDF C
 export CFLAGS=-fPIC 
-export CPPFLAGS=-I/$DIR/hdf5/include 
-export CPPFLAGS=-I/$DIR/curl/include 
+export CPPFLAGS='-I/$DIR/hdf5/include -I/$DIR/curl/include'
+export LDFLAGS='-L/$DIR/hdf5/lib -L/$DIR/curl/lib'
 # NetCDF fortran
-export CPPFLAGS=-I/$DIR/netcdf/include
-export LDFLAGS=-L/$DIR/netcdf/lib 
-export FCFLAGS=-m64
-
+export CPPFLAGS='-I/$DIR/netcdf/include'
+export LDFLAGS='-L/$DIR/netcdf/lib' 
+export FCFLAGS='-m64'
+#
 export PATH=$DIR/netcdf/bin:$PATH
 export NETCDF=$DIR/netcdf
-export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$DIR/netcdf/lib:$LD_LIBRARY_PATH
+#________________________
 # MPICH
 export PATH=$DIR/mpich/bin:$PATH
 export FFLAGS=-fallow-argument-mismatch 
 export FCFLAGS=-fallow-argument-mismatch 
+#________________________
 # libpng
-export CPPFLAGS=-I/$DIR/netcdf/include 
-export LDFLAGS=-L$DIR/netcdf/lib
-export FCFLAGS='-m64'
+export CPPFLAGS='-I/$DIR/netcdf/include'
+export FCFLAGS='- m64'
+#________________________
 # JasPer
 export PATH=$DIR/jasper/bin:$PATH
 export LD_LIBRARY_PATH=$DIR/jasper/lib:$LD_LIBRARY_PATH
-
+#________________________
 # WRF v4.4
 export WRF_DIR=$HOME/Build_WRF/WRFV4.4
 export MALLOC_CHECK_=0
@@ -58,8 +61,8 @@ export PATH=$KPP_HOME/bin:$PATH
 export SED=/usr/bin/sed
 export WRFIO_NCD_LARGE_FILE_SUPPORT=1
 # WPS-4.4
-export JASPERLIB=/home/Build_WRF/LIBRARIES/jasper/lib 
-export JASPERINC=/home/Build_WRF/LIBRARIES/jasper/include
+export JASPERLIB=$DIR/jasper/lib 
+export JASPERINC=$DIR/jasper/include
 #________________________________________________________
 # Aplicar los cambios al sistema con le siguiente comando:
 # source ~/.bashrc
