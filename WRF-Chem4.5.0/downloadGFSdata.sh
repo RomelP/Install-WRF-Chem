839627ec9da69a44e93679d46a834cc697ee4489
Romel Príncipe <57165643+RomelP@users.noreply.github.com>
2023-08-05 00:09:40 -0500

    Add files via upload

---
+++ WRF-Chem4.5.0/downloadGFSdata.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#----------------------------------------------------------
+# download folder
+cd "$WRFCHEM_HOME/DATA" || exit
+
+# download date
+year  = '2023'
+month = "07
+day  = '27' 
+hour ='00'
+
+# clear previos download
+
+rm -rf gfs.t${hour}z*
+
+#----------------------------------------------------------
+# Download hourly forescast data for the next 2 days
+
+fHour=0
+
+while [$fHour -le 24]:do
+
+    [$fHour -lt 10] && k='00' || k = '0'
+    wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.Op25.f${k}{fHour}
+    fHours = ${(fHour + 1)}
+
+    done
+
+#----------------------------------------------------------
