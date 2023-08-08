176b6c87263f413f2c51cd79bc34c971d698cb13
Romel Príncipe <57165643+RomelP@users.noreply.github.com>
2023-08-08 15:43:23 -0500

    Update downloadGFSdata.sh

--- WRF-Chem4.5.0/downloadGFSdata.sh
+++ WRF-Chem4.5.0/downloadGFSdata.sh
@@ -19,7 +19,7 @@ fHour=0
 while [$fHour -le 24]
 do
     [$fHour -lt 10] && k='00' || k='0'
-    wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.Op25.f${k}${fHour}
+    wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.0p25.f${k}${fHour}
     fHours=$(($fHour + 1))
 done
 echo "Out of the loop !!!"
