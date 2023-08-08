0aa660d1d488358540a31d961730edda4660f637
Romel Príncipe <57165643+RomelP@users.noreply.github.com>
2023-08-08 16:10:09 -0500

    Update downloadGFSdata.sh

--- WRF-Chem4.5.0/downloadGFSdata.sh
+++ WRF-Chem4.5.0/downloadGFSdata.sh
@@ -16,9 +16,9 @@ rm -rf gfs.t${hour}z*
 # Download hourly forescast data for the next 2 days
 
 fHour=0
-while [$fHour -le 24]
+while [ $fHour -le 24 ]
 do
-    [$fHour -lt 10] && k='00' || k='0'
+    [ $fHour -lt 10 ] && k='00' || k='0'
     wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.0p25.f${k}${fHour}
     fHours=$(($fHour + 1))
 done
