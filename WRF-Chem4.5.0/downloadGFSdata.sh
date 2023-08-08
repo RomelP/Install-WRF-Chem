886b9b7ee038eef253e7f3b06db4fb56933aaada
Romel Príncipe <57165643+RomelP@users.noreply.github.com>
2023-08-08 11:37:46 -0500

    Update downloadGFSdata.sh

--- WRF-Chem4.5.0/downloadGFSdata.sh
+++ WRF-Chem4.5.0/downloadGFSdata.sh
@@ -18,7 +18,7 @@ rm -rf gfs.t${hour}z*
 fHour=0
 while [$fHour -le 24]
 do
-    [$fHour -lt 10] && k='00' || k = '0'
+    [$fHour -lt 10] && k='00' || k='0'
     wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.Op25.f${k}${fHour}
     fHours=$((fHour + 1))
 done
