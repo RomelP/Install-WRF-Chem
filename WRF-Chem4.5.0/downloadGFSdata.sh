e9a03c4a7c3b14b57c4647ddc602d309192448f1
Romel Príncipe <57165643+RomelP@users.noreply.github.com>
2023-08-08 17:45:14 -0500

    Update downloadGFSdata.sh
    Cambio final

--- WRF-Chem4.5.0/downloadGFSdata.sh
+++ WRF-Chem4.5.0/downloadGFSdata.sh
@@ -3,24 +3,24 @@
 #----------------------------------------------------------
 # download folder
 cd "$WRFCHEM_HOME/DATA" || exit
+#----------------------------------------------------------
 # download date
 echo "date today!!! "
 read -p "year: " year   #year='2023'
 read -p "month: " month #month='08'
 read -p "day: " day     #day='08' 
 read -p "hour: " hour   #hour='00'
- 
+#----------------------------------------------------------
 # clear previos download
 rm -rf gfs.t${hour}z*
 #----------------------------------------------------------
 # Download hourly forescast data for the next 2 days
-
 fHour=0
 while [ $fHour -le 24 ]
 do
-    [ $fHour -lt 10 ] && k='00' || k='0'
-    wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.0p25.f${k}${fHour}
-    fHours=$(($fHour+1))
+  [ $fHour -lt 10 ] && k='00' || k='0'
+  wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.${year}${month}${day}/${hour}/atmos/gfs.t${hour}z.pgrb2.0p25.f${k}${fHour}
+  fHours=$(($fHour+1))
 done
 echo "Out of the loop !!!"
 #----------------------------------------------------------
